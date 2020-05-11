fd98c49a79fa714ccd48bb9406ef4ef0117043a0
kelvin53 <geshkoskei53@gmail.com>
2020-05-11 13:00:58 +0300

    added toggle effect on what-we-do section

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,25 @@
 $(document).ready(function(){
 
+//what-we-do toggle effect
+    $("#design").click(function() {
+        $("#design p").toggle('1500');
+        $("#design img").toggle('1000');
+      });
+      
+      $("#development").click(function() {
+        $("#development p").toggle('1500');
+        $("#development img").toggle('1000');
+        
+      });
+      
+      $("#product").click(function() {
+        $("#product p").toggle('1500');
+        $("#product img").toggle('1000');
+      });
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
 });
